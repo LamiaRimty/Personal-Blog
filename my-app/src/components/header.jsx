2841b47f5d6e3910f1bc3a68364802b67414029c
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-02-16 16:53:50 +0100

    added navbar collapse

--- my-app/src/components/header.jsx
+++ my-app/src/components/header.jsx
@@ -1,43 +1,51 @@
 function Header(){
 return(
-  <div className="navbar">
-     {/* <!-- Nav Bar --> */}
-<nav class="navbar navbar-nav navbar-expand-lg navbar-dark ">
+
+   <div class="navbar navbar-nav navbar-expand-lg navbar-dark ">
 
   <div class="container">
     <div class="navbar-header">
-      <p class="navbar-brand" href="">👩🏻‍💻 Memoir</p>
+      <p class="navbar-brand" href="">
+       <img className="img" src="https://i.pinimg.com/originals/05/d9/cd/05d9cdb4a4be873427b638610ca68292.jpg" alt="tulip"/>
+        Memoir
+        </p>
     </div>
     
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
       aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-    </button>
+      </button>
+     
+ <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
 
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+<ul class="nav navbar-nav ms-auto ">
+  <li id="home">
+    <a class="nav-link" href="/">HOME</a>
+  </li>  
 
-      <ul class="nav navbar-nav ms-auto ">
-        <li id="home">
-          <a class="nav-link" href="/">HOME</a>
-        </li>  
+    <li id="blogs">
+    <a class="nav-link" href="/blogs">BLOGS</a>
+  </li> 
 
-          <li id="blogs">
-          <a class="nav-link" href="/blogs">BLOGS</a>
-        </li> 
+  <li id="about">
+    <a class="nav-link" href="/about">ABOUT</a>
+  </li>
+  <li id="contact">
+    <a class="nav-link" href="/contact">CONTACT</a>
+  </li>
+</ul>
+<i className="fas fa-search"></i>
+</div>
+   
+    
 
-        <li id="about">
-          <a class="nav-link" href="/about">ABOUT</a>
-        </li>
-        <li id="contact">
-          <a class="nav-link" href="/contact">CONTACT</a>
-        </li>
-      </ul>
-    </div>
+
   </div>
-</nav>
-  </div>
-);
+</div>
+); 
 }
+
+
   
 export default Header;
 
