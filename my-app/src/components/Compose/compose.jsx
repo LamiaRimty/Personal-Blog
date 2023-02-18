410496e71db36a41c0f6bc5093ca4f792a1d5bd5
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-02-18 21:00:05 +0100

    Add new blog Compose page

--- my-app/src/components/Compose/compose.jsx
+++ my-app/src/components/Compose/compose.jsx
@@ -2,7 +2,20 @@ import "./compose.css"
 function Compose(){
     return(
         <div className="compose">
-            hello
+            <form className="composeForm">
+               <div className="composeFormGroup">
+                    <label>Title</label>
+                    <input className="composeBlog" id="title" type="text" placeholder="Title" autoFocus={true}/>
+                </div> 
+
+                <div className="composeFormGroup"> 
+                    <label>Description</label>
+                    <textarea className="composeBlog" id="description" type="text" placeholder="Write a blog..."></textarea>
+                </div> 
+
+                <button className="composeBlogBtn"  type="submit" autocomplete="off">Publish</button>
+
+            </form>
         </div>
     );
 }
