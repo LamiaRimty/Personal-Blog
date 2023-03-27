f934d306a80ce8690b96ee65e76d8c1a5a2af59e
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-03-27 14:59:02 +0200

    Successfully connected to the database

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
-const dotenv=require("dotenv")
+const dotenv = require("dotenv");
+dotenv.config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true})
-.then(console.log("Connect to MongoDB"))
-.catch(err => console.log(err));
+
+mongoose.connect(`${process.env.MONGO_URL}`,
+  {
+    useUnifiedTopology:true,
+    useNewUrlParser: true,
+    
+  }
+  )
+  .then(() => {
+    console.log("Successfully connected to DB");
+  })
+  .catch((error) => {
+    console.log(`can not connect to database, ${error}`);
+  });
+
 
 app.listen(3000, () => {
   console.log(`Backend server listening`)
