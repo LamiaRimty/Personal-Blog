9b036a80d7c1dfc051042f724fc322226e2cada2
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-03-28 12:38:59 +0200

    handling array to blogData through route parameter

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,49 @@
 const express = require('express')
-const app = express()
+const app = express();
+
+const blogs = [
+  {
+    id: 1,
+    image:"https://c.stocksy.com/a/2bL800/z9/1989644.jpg",
+    title: "My journey to Web Development",
+    time: "December 1,2022 .  🥐 10 min to read",
+    qoute: "A personal reflection",
+    description:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+  },
+
+  {
+    id: 2,
+    image:"https://eatwelltraveloften.online/wp-content/uploads/2016/08/IMG_0083-1080x720.jpg",
+    title: "Difference between == and === in javascript",
+    time: "December 1,2022 .  ☕️ 10 min to read",
+    qoute: "Which one to use?",
+    description:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+  },
+
+  {
+    id: 3,
+    image:"https://media.istockphoto.com/id/516019979/photo/amsterdam-summer-sunrise.jpg?s=612x612&w=0&k=20&c=YnYn52rR0J_YINkjK8JPMTfnrqRn1E2AzlLayNOgnC4=",
+    title: "Ways of Creating objects JavaScript",
+    time: "December 1,2022 .  🍱 10 min to read",
+    qoute: "Three ways to set you on right path.?",
+    description:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+  },
+]
 
 app.get("/",(req,res)=>{  //route handler function
   res.send("Hey There!!!!");
 })
 
 app.get("/backend/blogs",(req,res)=>{
-  res.send(
-    [
-      {id:1}, 
-     { id:2},
-     { id:3}
-    ]
-  );
+  res.send(blogs);
 })
 
 app.get("/backend/blogs/:id",(req,res)=>{ 
-  res.send(req.params.id)
-})
-
-app.get("/backend/blogs/:year/:month/:day",(req,res)=>{ 
-  res.send(req.query)
+  const id= blogs.find((b)=>b.id=== parseInt(id));
+  res.send(id);
 })
 
 
