63edc46f3dfa9f328aa5153ff49efb21d7280f6f
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-03-28 11:36:06 +0200

    At first create Endpoints using HTTP method

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,39 @@
 const express = require('express')
 const app = express()
-const dotenv = require("dotenv");
-dotenv.config();
-const mongoose = require('mongoose');
-app.use(express.json())
 
-mongoose.connect(`${process.env.MONGO_URL}`,
-  {
-   
-    useNewUrlParser: true,
+app.get("/",(req,res)=>{  //route jandler function
+  res.send("Hey There!");
+})
+
+app.get("/backend/blogs",(req,res)=>{
+  res.send(
+    [
+      {id:1},
+     { id:2},
+     { id:3}
+    ]
+  );
+})
+
+
+// const dotenv = require("dotenv");
+// dotenv.config();
+// const mongoose = require('mongoose');
+// app.use(express.json())
+
+// mongoose.connect(`${process.env.MONGO_URL}`,
+//   {
+//     useUnifiedTopology:true,
+//     useNewUrlParser: true,
     
-  }
-  )
-  .then(() => {
-    console.log("Successfully connected to Database");
-  })
-  .catch((error) => {
-    console.log(`can not connect to database, ${error}`);
-  });
+//   }
+//   )
+//   .then(() => {
+//     console.log("Successfully connected to Database");
+//   })
+//   .catch((error) => {
+//     console.log(`can not connect to database, ${error}`);
+//   });
 
 
 app.listen(3000, () => {
