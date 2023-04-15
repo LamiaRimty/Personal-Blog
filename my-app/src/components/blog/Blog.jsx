0c3815b18e43df05c0aac7c58ac13a92a4894b22
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-04-15 13:19:39 +0200

    add publicFolder to blog.jsx

--- my-app/src/components/blog/Blog.jsx
+++ my-app/src/components/blog/Blog.jsx
@@ -1,3 +1,4 @@
+//Documentation style
 // import "./blog.css";
 // import React from "react";
 // import { Link } from "react-router-dom";
@@ -41,6 +42,7 @@
 //   );
 // }
 // export default Blog;
+
 import "./blog.css";
 import React from "react";
 import { Link } from "react-router-dom";
@@ -49,6 +51,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function Blog() {
+  const publicFolder = "localhost:/8000/images/";
   const [blogs, setBlogs] = useState([]);
   const fetchBlogs = async () => {
     const responseBlog = await axios.get(`http://localhost:8000/backend/blogs`);
@@ -66,7 +69,7 @@ function Blog() {
           {blogs.map((b) => (
             <article className="blogItems" key={b.id}>
               <div className="blog-img">
-                <img src={b.image} alt="img" />
+                <img src={publicFolder + b.image} alt="img" />
               </div>
               <Link to={`details/${b.id}`} className="link">
                 <h3 className="title">{b.title}</h3>
