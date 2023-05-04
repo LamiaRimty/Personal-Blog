86521387d154cd445449276f926bb7a964d1e965
Lamia Jabin Rimty <lamiajabinrimty@Lamias-Air.home>
2023-05-04 11:22:07 +0200

    Create mongoose schema

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,17 @@ app.use(express.json()); //json returns middleware
 const multer = require("multer")
 const path = require("path")
 app.use("/images", express.static(path.join(__dirname, "/images")))
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/blogDB');
+
+const blogSchema = new mongoose.Schema({
+  image:String,
+  title:String,
+  time:String,
+  qoute:String,
+  description:String
+});
+
 
 const blogs = [
   {
@@ -50,9 +61,9 @@ const blogs = [
   
     // id:5,
     // image:"https://c8.alamy.com/comp/W06HG1/chawk-bazar-iftar-market-of-dhaka-is-well-known-for-traditional-spicy-foods-thousands-of-people-gathered-on-a-road-in-front-of-shahi-mosque-where-sel-W06HG1.jpg",
-    // title:"Chawkbazar: The 400-year-old bustling heart of Dhaka.",
+    // title:"Chawkbazar: The 400-year-old bustling heart of Dhaka",
     // time:"⏱21 February 2019🧆🧃.5mins to read.",
-    // qoute:"Chawkbazar is popular during Ramadan, the Muslim month of fasting, to break fast.",
+    // qoute:"Chawkbazar is popular during Ramadan, the Muslim month of fasting, to break fast",
     // description:"During the Muslim holy month of Ramadan, Chawkbazar - which is popular for various types of traditional food - turns into the main place for Iftar items - the meal which breaks the daily fast. Thousands of vendors peddle their goods on its streets, often filled with people waiting to get their fill of kebabs and different types of biryani - a popular rice dish. They compete for space with the hundreds of electrical, telephone, and internet cables that hang on to its narrow lanes, posing another danger for the local people in Chawkbazar. "
   
   
@@ -76,88 +87,39 @@ app.get("/backend/blogs/:id",(req,res)=>{
   } 
   res.send(blog);
 })
-// //Upload images [From Documentation]
-// const storage = multer.diskStorage({
-//   destination: (req, file, callb) => {
-//     callb(null, "images")
-//   },
-  
-//   filename: (req, file, callb) => {
-//     //callb(null, "file.png")
-//     callb(null, req.body.name)
-//   },
-  
-// })
 
-// console.log("10");
-
-// const upload = multer({ dest: "blogs/" })
-// console.log("a")
-// app.post("/backend/blogs", upload.single("image"),(req,res)=>{
-  
-//   console.log("b")
-
-//   const result =validateBlog(req.body);
-//   const {error} =validateBlog(req.body); // 3.validate
-//   console.log("c");
-//   if(error){
-//     console.log(error);
-//   return  res.status(400).send(error.details[0].message);  // 4.if invalid,return 400 bad req
-  
-//   }
-//   console.log("e");
-//   const blog ={
-//     id: blogs.length+1,
-//     name:req.file.filename,
-//     image: req.body.file,
-//     title: req.body.title,
-//     time: req.body.time,
-//     qoute:req.body.qoute,
-//     description:req.body.description
- 
-//   };
-  
-//   console.log("f");
-//   blogs.push(blog);
-//   res.send(blog);
-//   console.log("g");
-
-
-// });
-// console.log("h");
-//////////////////////\\\\\\\\\\\\\\\\\\\
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "images");
-    console.log("10");
+
   },
   filename: (req, file, cb) => {
     cb(null, req.body.name);
   },
   
 });
-console.log("20");
+
 
 const upload = multer({ storage: storage });
-console.log("30");
+
 app.post("/backend/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
-console.log("40");
+
 
 
 
 app.post("/backend/blogs",(req,res)=>{
   const result =validateBlog(req.body);
   const {error} =validateBlog(req.body); // 3.validate
-  console.log("50");
+ 
   if(error){
     console.log(error)
   return  res.status(400).send(error.details[0].message);  // 4.if invalid,return 400 bad req
   
   }
-  console.log("60");
+  
   const blog ={
     id: blogs.length+1,
     image: req.body.image,
@@ -166,45 +128,59 @@ app.post("/backend/blogs",(req,res)=>{
     qoute:req.body.qoute,
     description:req.body.description
   };
-  console.log("70");
+
   blogs.push(blog);
   res.send(blog);
-  console.log("80");
+
 });
-
-console.log("90");
-
-
-
 
 //UPDATING BLOG POST
 app.put("/backend/blogs/:id",(req,res)=>{
- 
+  console.log(10);
   const blog = blogs.find( c => c.id ===parseInt(req.params.id)); //1.look up the blog
+  console.log(20);
   if(!blog){ //if don't hav a blog with given id
+    console.log(30);
    return res.status(404).send("Sorry this blog with given the id is not available!"); //2.if not exit ,return 404
   }
-
+  console.log(40);
   const {error} =validateBlog(req.body);
-
+  console.log(50);
    if(error){
+    console.log(error)
     return res.status(400).send(error.details[0].message);
   
    }
-
+   console.log(60);
 // 5,update log
 blog.title = req.body.title;
 blog.image = req.body.image;
-blog.time= req.body.image;
+blog.time= req.body.time;
 blog.qoute = req.body.qoute;
 blog.description= req.body.description;
 
-
+console.log(70);
 // 6.return the update course
 res.send(blog);
+console.log(80);
 })
+///valid for put
+// function validateBlogPut(blog){
+//   const schema = Joi.object({
+    
+  
+//   title:Joi.string().min(3).required(),
 
+//   description:Joi.string().required()
+  
+// })
+// console.log(90);
+// return schema.validate(blog);
 
+// }
+
+console.log(100);
+ 
 
 app.delete("/backend/blogs/:id",(req,res)=>{ 
 //Look up the course
@@ -237,25 +213,6 @@ return schema.validate(blog);
 
 }
 
-// const dotenv = require("dotenv");
-// dotenv.config();
-// const mongoose = require('mongoose');
-
-
-// mongoose.connect(`${process.env.MONGO_URL}`,
-//   {
-//     useUnifiedTopology:true,
-//     useNewUrlParser: true,
-    
-//   }
-//   )
-//   .then(() => {
-//     console.log("Successfully connected to Database");
-//   })
-//   .catch((error) => {
-//     console.log(`can not connect to database, ${error}`);
-//   });
-
 
 const cors=require("cors");
 const corsOptions ={
@@ -271,3 +228,5 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Backend server listening on ${port}...`);
 })
+
+
