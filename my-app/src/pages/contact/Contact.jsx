4a52d571e2fd608218fed844bee220ff01218c12
Lamia Jabin Rimty <lamiajabinrimty@Lamias-Air.home>
2023-05-26 13:00:25 +0200

    Add contact image

--- my-app/src/pages/contact/Contact.jsx
+++ my-app/src/pages/contact/Contact.jsx
@@ -13,71 +13,82 @@ function Contact() {
       </h5>
       <div className="container contact-container">
         {/* <h3>Get In Touch</h3> */}
-        <form className="form" action="">
-          <label>
-            <span>Your Email:</span>
-            <input
-              type="text"
-              name="name"
-              placeholder="Your first name"
-              required
-            />
-          </label>
+        <div>
+          <form className="form" action="">
+            <label>
+              <span>Your Email:</span>
+              <input
+                type="text"
+                name="name"
+                placeholder="Your first name"
+                required
+              />
+            </label>
 
-          <label>
-            <span>Your Email:</span>
-            <input
-              type="email"
-              name="email"
-              placeholder="Your email address"
-              required
-            />
-          </label>
+            <label>
+              <span>Your Email:</span>
+              <input
+                type="email"
+                name="email"
+                placeholder="Your email address"
+                required
+              />
+            </label>
 
-          <label>
-            <span>Your Massage:</span>
-            <textarea
-              id="message"
-              type="message"
-              name="message"
-              placeholder="Your message"
-              required
-              rows="5"
-              cols="6"
-            ></textarea>
-          </label>
+            <label>
+              <span>Your Massage:</span>
+              <textarea
+                id="message"
+                type="message"
+                name="message"
+                placeholder="Your message"
+                required
+                rows="5"
+                cols="6"
+              ></textarea>
+            </label>
 
-          <button className="contactBtn" type="submit">
-            Send
-          </button>
-        </form>
+            <button className="contactBtn" type="submit">
+              Send
+            </button>
+          </form>
+        </div>
+
+        <div>
+          <img
+            className="contact-img"
+            // src="https://d2gg9evh47fn9z.cloudfront.net/1600px_COLOURBOX56391809.jpg"
+            src="https://cdn-icons-png.flaticon.com/512/5075/5075578.png"
+            alt="rimty"
+          />
+        </div>
 
         <div className="social-contact">
           {/* <h3>Get Social</h3> */}
           <a
             href="https://www.linkedin.com/in/lamia-jabin-rimty-0020a2219/"
             target="_blank"
+            rel="noreferrer"
           >
             <BsLinkedin />
           </a>
-          <a href="https://github.com/LamiaRimty" target="_blank">
+          <a
+            href="https://github.com/LamiaRimty"
+            target="_blank"
+            rel="noreferrer"
+          >
             <BsGithub />
           </a>{" "}
           <a
             href="https://api.whatsapp.com/send?phone=0626344867"
             target="_blank"
+            rel="noreferrer"
           >
             <IoLogoWhatsapp />
           </a>
-          <a href="mailto:ljrimty@outlook.com" target="_blank">
+          <a href="mailto:ljrimty@outlook.com" target="_blank" rel="noreferrer">
             <AiOutlineMail />
           </a>
-        </div>
-        <div className="falcon falcon--right-side">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7M89k7N4TAsCqVyzXYDUkBBh4_ISBYrCJjA&usqp=CAU"
-            className="falcon__inner"
-          ></img>
         </div>
       </div>
     </section>
