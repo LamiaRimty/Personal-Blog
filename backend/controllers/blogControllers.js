f7691fb58fa41e32aeec20fa66fc032b363f97bc
Lamia Jabin Rimty <lamiajabinrimty@Lamias-Air.home>
2023-06-09 12:32:09 +0200

    Add blogControllers & building out rest of API

--- backend/controllers/blogControllers.js
+++ backend/controllers/blogControllers.js
@@ -1,13 +1,19 @@
 //config db & connect
-
+const Blog = require("../models/Blog");
 exports.getAllBlogs=  async(req,res,next)=>{
     res.send("Get all blogs route");
 }
 
 exports.createNewBlog=  async(req,res,next)=>{
-    res.send("Create new blog route");
+    let { image,title,time,qoute,description } =req.body; //stract frpm postman
+    let blog =new Blog(image,title,time,qoute,description);
+
+    blog=await blog.save();
+    console.log(blog);
+
+    res.send("Create New Blog route");
 }
 
 exports.getBlogById=  async(req,res,next)=>{
-    res.send("Get blog by id");
+    res.send("Get Blog by id");
 }
